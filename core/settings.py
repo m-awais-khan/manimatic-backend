9c131898ecac4139c7bcb26e4085b1a438ef79f7
Awais Khan <468awaiskhan2003@gmail.com>
2026-07-27 18:30:17 +0500

    Apply security patches to Django settings

--- core/settings.py
+++ core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-or#n*_@gbdduc$w2$ye)qwi0@o&^fbxh%ndr&vz50sn1w&^-s8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-or#n*_@gbdduc$w2$ye)qwi0@o&^fbxh%ndr&vz50sn1w&^-s8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*'] # Allowed absolutely everywhere for ease of deployment
 
